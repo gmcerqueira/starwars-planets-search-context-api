1664761f46a4cb94ea7777f4c629d93a9c102fd2
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-06-28 16:03:05 -0300

    realoc contex from App to Provider

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
 import PlanetsProvider from './context/PlanetsProvider';
 
 function App() {
-  const [data, setData] = useState({});
-
-  const fetchPlanets = async () => {
-    const PLANETS_URL = 'https://swapi-trybe.herokuapp.com/api/planets/';
-
-    const planets = await fetch(PLANETS_URL)
-      .then((res) => res.json())
-      .then(({ results }) => results);
-
-    return planets;
-  };
-
-  useEffect(() => {
-    const fetchPage = async () => {
-      const planets = await fetchPlanets();
-      setData(planets);
-    };
-    fetchPage();
-  }, []);
-
-  const context = { data };
   return (
-    <PlanetsProvider value={ context }>
+    <PlanetsProvider>
       <span>Hello, App!</span>
     </PlanetsProvider>
   );
--- src/context/PlanetsProvider.jsx
+++ src/context/PlanetsProvider.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import PlanetsContext from './PlanetsContext';
 
-const PlanetsProvider = ({ children }) => (
-  <PlanetsContext.Provider>{children}</PlanetsContext.Provider>
-);
+const PlanetsProvider = ({ children }) => {
+  const [data, setData] = useState({});
+
+  const fetchPlanets = async () => {
+    const PLANETS_URL = 'https://swapi-trybe.herokuapp.com/api/planets/';
+
+    const planets = await fetch(PLANETS_URL)
+      .then((res) => res.json())
+      .then(({ results }) => results);
+
+    setData(planets);
+  };
+
+  useEffect(() => {
+    fetchPlanets();
+  }, []);
+
+  const context = { data };
+  return (
+    <PlanetsContext.Provider value={ context }>{children}</PlanetsContext.Provider>
+  );
+};
 
 PlanetsProvider.propTypes = {
   children: PropTypes.node.isRequired,
