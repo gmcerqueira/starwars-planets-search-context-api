d94ba81901e94272e08d0f307180241c850eee67
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-06-28 16:50:27 -0300

    show data table with planets info

---
+++ src/components/DataTable.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TableBody from './TableBody';
+import TableHeader from './TableHeader';
+
+const DataTable = () => (
+  <table>
+    <TableHeader />
+    <TableBody />
+  </table>
+);
+
+export default DataTable;
