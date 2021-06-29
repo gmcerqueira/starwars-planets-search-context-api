114be4236d7f8128c1f6c53d32c2ce8b8075b0e3
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-06-28 21:05:39 -0300

    select inputs values for number filter

---
+++ src/components/filters/DataFilters.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FilterByName from './FilterByName';
+import FilterByNumber from './FilterByNumber';
+
+const DataFilters = () => (
+  <div>
+    <FilterByName />
+    <FilterByNumber />
+  </div>
+);
+
+export default DataFilters;
---
+++ src/components/filters/FilterByNumber.jsx
@@ -0,0 +1,76 @@
+import React, { createRef, useContext } from 'react';
+import PlanetsContext from '../../context/PlanetsContext';
+
+const FilterByNumber = () => {
+  const { saveFilter } = useContext(PlanetsContext);
+
+  const renderOptions = (options) => options.map((option) => (
+    <option key={ option } value={ option }>
+      {option}
+    </option>
+  ));
+
+  const columnOptions = [
+    'population',
+    'orbital_period',
+    'diameter',
+    'rotation_period',
+    'surface_water',
+  ];
+  const comparisonOptions = ['maior que', 'menor que', 'igual a'];
+  const columnSelector = createRef();
+  const comparisonSelector = createRef();
+  const valueSelector = createRef();
+
+  return (
+    <div>
+      <label htmlFor="column-filter">
+        Column:
+        <select
+          name="column-filter"
+          id="column-filter"
+          ref={ columnSelector }
+          data-testid="column-filter"
+        >
+          {renderOptions(columnOptions)}
+        </select>
+      </label>
+
+      <label htmlFor="comparison-filter">
+        Comparison:
+        <select
+          name="comparison-filter"
+          id="comparison-filter"
+          ref={ comparisonSelector }
+          data-testid="comparison-filter"
+        >
+          {renderOptions(comparisonOptions)}
+        </select>
+      </label>
+
+      <label htmlFor="value-filter">
+        Value:
+        <input
+          type="number"
+          name="value-filter"
+          id="value-filter"
+          ref={ valueSelector }
+          data-testid="value-filter"
+        />
+      </label>
+      <button
+        type="button"
+        data-testid="button-filter"
+        onClick={ () => saveFilter(
+          columnSelector.current.value,
+          comparisonSelector.current.value,
+          valueSelector.current.value,
+        ) }
+      >
+        Add filter
+      </button>
+    </div>
+  );
+};
+
+export default FilterByNumber;
