4db03bb69a1a4d1e69caddffa3757e1f3105bf73
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-07-25 18:04:54 -0300

    Updates

---
+++ precache-manifest.09e27474b186c535cecbaa17a9cd9098.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "177f458a3eaead8278a0f574fbeabdd4",
+    "url": "/starwars-planets-search-context-api/index.html"
+  },
+  {
+    "revision": "6f8e89f054004973a74a",
+    "url": "/starwars-planets-search-context-api/static/css/main.0c8dd27f.chunk.css"
+  },
+  {
+    "revision": "76d4875f930eb24c0584",
+    "url": "/starwars-planets-search-context-api/static/js/2.017d0772.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/starwars-planets-search-context-api/static/js/2.017d0772.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f8e89f054004973a74a",
+    "url": "/starwars-planets-search-context-api/static/js/main.bae8bf38.chunk.js"
+  },
+  {
+    "revision": "e8b32013c019444adc55",
+    "url": "/starwars-planets-search-context-api/static/js/runtime-main.23e8910c.js"
+  }
+]);
